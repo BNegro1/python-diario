1e9167273dee1ebc96549620a79ec454b011553a
BNegro1 <72948318+BNegro1@users.noreply.github.com>
2024-07-03 18:17:34 -0400

    Step 1 realizado

---
+++ Scientific Computing Python - FCC/Paso 2.py
@@ -0,0 +1,4 @@
+'''
+Variables can store values of different data types. You just assigned an integer value, but if you want to represent some text, you need to assign a string. Strings are sequences of characters enclosed by single or double quotes, but you cannot start a string with a single quote and end it with a double quote or vice versa:
+
+'''
