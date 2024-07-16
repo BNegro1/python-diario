a86dff8081729237873533b8089717ac46bc6e9b
BNegro1 <72948318+BNegro1@users.noreply.github.com>
2024-07-15 23:14:26 -0400

    Update Step 6.py

--- Scientific Computing Python - FCC/Step 6.py
+++ Scientific Computing Python - FCC/Step 6.py
@@ -5,5 +5,5 @@ You can also access string characters starting from the end of the string. The l
 Now modify your existing print() call to print the last character in your string.
 '''
 
-text = 'Hello World'
+text = 'Hello World aa'
 print(text[-1])
