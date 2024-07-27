4458129e89961213e508e692a559a99699632b37
BNegro1 <72948318+BNegro1@users.noreply.github.com>
2024-07-27 19:43:51 -0400

    Ejercicio 10.py y 11.py

---
+++ MoureDev/10.py
@@ -0,0 +1,7 @@
+'''
+Diseña un algoritmo que calcule el IVA sujeto al
+16% de un producto dado su precio de venta sin IVA.
+'''
+iva = 0.16
+prod = int(input("Ingrese precio del producto: "))
+print(f'Precio de venta: {iva*prod}')
---
+++ MoureDev/11.py
@@ -0,0 +1,16 @@
+'''
+Traduce las siguientes expresiones 
+matematicas a Python y eval ualas. 
+Trata de utilizar el menor n ́umero de parentesis posible.
+'''
+a = 2+(3*(6/2))
+print(a)
+b = 4+6/2+3
+print(b)
+c = (4/2)**5
+# Test: c = 4**(1/2)
+print(c)
+d = (4/2)**(5)+1
+print(d)
+e = (-3)**2 # Inversa = 9; Potencia neg. dentro de paréntesis.
+print(e)
