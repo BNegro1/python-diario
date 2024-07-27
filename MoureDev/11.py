3ea19c23f60248c978721ccca87c051fe2261c9b
BNegro1 <72948318+BNegro1@users.noreply.github.com>
2024-07-27 19:45:34 -0400

    Actualización 11.py final

--- MoureDev/11.py
+++ MoureDev/11.py
@@ -5,12 +5,12 @@ Trata de utilizar el menor n ́umero de parentesis posible.
 '''
 a = 2+(3*(6/2))
 print(a)
-b = 4+6/2+3
+b = (4+6)/(2+3)
 print(b)
 c = (4/2)**5
 # Test: c = 4**(1/2)
 print(c)
-d = (4/2)**(5)+1
+d = (4/2)**(6)
 print(d)
 e = (-3)**2 # Inversa = 9; Potencia neg. dentro de paréntesis.
 print(e)
